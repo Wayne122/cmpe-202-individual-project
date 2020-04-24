1480a7b1f6e6604232a949dac2b6b193bf9dd698
Wayne122 <ww040702tw01@gmail.com>
2020-04-23 21:48:32 -0700

    Fix CSVReader for different order of columns.

--- src/main/java/com/cmpe202ip/model/CCInput/CSVReader.java
+++ src/main/java/com/cmpe202ip/model/CCInput/CSVReader.java
@@ -8,20 +8,28 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class CSVReader implements ReadingStrategy {
   @Override
   public ArrayList<CreditCard> read(String filePath) throws IOException {
     ArrayList<CreditCard> creditCards = new ArrayList<>();
     BufferedReader br = new BufferedReader(new FileReader(filePath));
-    String line = "";
+    String line;
     String cvsSplitBy = ",";
     CCRecordHandler handler = HandlerGetter.getInstance();
-    br.readLine();
+    line = br.readLine();
+    String[] columns = line.split(cvsSplitBy);
+    HashMap<String, Integer> columnOrder = new HashMap<>();
+    Integer c = 0;
+    for (String column : columns) {
+      columnOrder.put(column, c);
+      c++;
+    }
     while ((line = br.readLine()) != null) {
       String[] ccRecord = line.split(cvsSplitBy);
-      //System.out.println("CardNumber= " + ccRecord[0] + ", ExpirationDate= " + ccRecord[1] + ", NameOfCardholder= " + ccRecord[2]);
-      CreditCard creditCard = handler.handleRequest(ccRecord[0], ccRecord[1], ccRecord[2]);
+      // System.out.println("CardNumber= " + ccRecord[0] + ", ExpirationDate= " + ccRecord[1] + ", NameOfCardholder= " + ccRecord[2]);
+      CreditCard creditCard = handler.handleRequest(ccRecord[columnOrder.get("CardNumber")], ccRecord[columnOrder.get("ExpirationDate")], ccRecord[columnOrder.get("NameOfCardholder")]);
       creditCards.add(creditCard);
     }
     br.close();
